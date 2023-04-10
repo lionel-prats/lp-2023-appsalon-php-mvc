bc34451998b22547ebefe8e5b541a1a4ea3b7ec8
lionel-prats <lionel.prats.c@gmail.com>
2023-04-10 01:32:57 -0300

    seccion 45 - video 446

--- public/index.php
+++ public/index.php
@@ -3,10 +3,24 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 
 $router = new Router();
 
+// iniciar sesion
+$router->get("/", [LoginController::class, "login"]); 
+$router->post("/", [LoginController::class, "login"]); 
+$router->post("/logout", [LoginController::class, "logout"]); 
 
+// recuperar password 
+$router->get("/olvide", [LoginController::class, "olvide"]); 
+$router->post("/olvide", [LoginController::class, "olvide"]); 
+$router->get("/recuperar", [LoginController::class, "recuperar"]); 
+$router->post("/recuperar", [LoginController::class, "recuperar"]); 
+
+// crear cuenta
+$router->get("/crear-cuenta", [LoginController::class, "crear"]); 
+$router->post("/crear-cuenta", [LoginController::class, "crear"]); 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
