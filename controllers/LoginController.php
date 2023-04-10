9e90688446c90a4fd2c777b8a5ad949a2b505aec
lionel-prats <lionel.prats.c@gmail.com>
2023-04-10 01:51:46 -0300

    seccion 46 - video 447

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,21 +2,23 @@
 
 namespace Controllers;
 
+//use MVC\Router;
+
 
 class LoginController {
-    public static function login() {
-        echo "Desde login";
+    public static function login(/* Router */ $router) {
+        $router->render("auth/login");
     }
     public static function logout() {
         echo "Desde logout";
     }
-    public static function olvide() {
+    public static function olvide(/* Router */ $router) {
         echo "Desde olvide";
     }
-    public static function recuperar() {
+    public static function recuperar(/* Router */ $router) {
         echo "Desde recuperar";
     }
-    public static function crear() {
+    public static function crear(/* Router */ $router) {
         echo "Desde crear";
     }
 }
