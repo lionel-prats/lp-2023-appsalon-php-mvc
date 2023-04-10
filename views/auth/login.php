dce746b11b39498d0cf6626114728c970e11a997
lionel-prats <lionel.prats.c@gmail.com>
2023-04-10 03:01:13 -0300

    seccion 46 - video 449

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,28 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form action="/" class="formulario" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="email"
+            id="email"
+            placeholder="tucorreo@correo.com"
+            name="email"     
+        >
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input 
+            type="password"
+            id="password"
+            placeholder="Ingresa tu contraseña"
+            name="password"     
+        >
+    </div>
+    <input type="submit" value="Iniciar Sesión" class="boton">
+</form>
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear una</a>
+    <a href="/olvide">¿Olvidaste tu password?</a>
+</div>
