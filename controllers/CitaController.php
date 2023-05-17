0beb4af8879ea57c63b85bd0d1d254230a1a8bad
lionel-prats <lionel.prats.c@gmail.com>
2023-05-17 02:22:36 -0300

    seccion 52 - video 488

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -6,8 +6,10 @@ use MVC\Router;
 
 class CitaController {
     public static function index(Router $router){
+        //session_start();
+        //debuguear($_SESSION);
         $router->render("cita/index", [
-            
+            "nombre" => $_SESSION["nombre"]
         ]);
     }
 }
--- views/cita/index.php
+++ views/cita/index.php
@@ -5,15 +5,41 @@
 <div id="app">
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
-        <p>Elige tus servicios a continuaciós</p>
+        <p class="text-center">Elige tus servicios a continuación</p>
         <div id="servicios" class="listado-servicios"></div>
     </div>
     <div id="paso-2" class="seccion">
         <h2>Tus Datos y Cita</h2>
-        <p>Coloca tus datos y fecha de tu cita</p>
+        <p class="text-center">Coloca tus datos y fecha de tu cita</p>
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input 
+                    id="nombre"
+                    type="text"
+                    placeholder="Tu nombre"
+                    value="<?=$nombre?>"
+                    disabled
+                >
+            </div>
+            <div class="campo">
+                <label for="fecha">Fecha</label>
+                <input 
+                    id="fecha"
+                    type="date"
+                >
+            </div>
+            <div class="campo">
+                <label for="hora">Hora</label>
+                <input 
+                    id="hora"
+                    type="time"
+                >
+            </div>  
+        </form>
     </div>
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
-        <p>Verifica que la información sea correcta</p>
+        <p class="text-center">Verifica que la información sea correcta</p>
     </div>
 </div>
