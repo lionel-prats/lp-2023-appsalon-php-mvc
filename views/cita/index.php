1de2a68c6bacd324d37957de8afe742d0423b090
lionel-prats <lionel.prats.c@gmail.com>
2023-05-22 00:09:39 -0300

    seccion 52 - video 489

--- views/cita/index.php
+++ views/cita/index.php
@@ -3,11 +3,21 @@
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
 <div id="app">
+
+    <!-- VIDEO 489 -->
+    <nav class="tabs">
+        <button type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="2">Información Cita</button>
+        <button type="button" data-paso="3">Resumen</button>
+    </nav>
+
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
         <p class="text-center">Elige tus servicios a continuación</p>
         <div id="servicios" class="listado-servicios"></div>
     </div>
+
+    <!-- VIDEO 488 - por defecto le da un display:none en _citas.scss -->
     <div id="paso-2" class="seccion">
         <h2>Tus Datos y Cita</h2>
         <p class="text-center">Coloca tus datos y fecha de tu cita</p>
@@ -18,7 +28,7 @@
                     id="nombre"
                     type="text"
                     placeholder="Tu nombre"
-                    value="<?=$nombre?>"
+                    value="<?php echo $nombre?>"
                     disabled
                 >
             </div>
@@ -38,8 +48,13 @@
             </div>  
         </form>
     </div>
+
+    <!-- VIDEO 488 - por defecto le da un display:none en _citas.scss -->
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
         <p class="text-center">Verifica que la información sea correcta</p>
     </div>
+
 </div>
+
+
