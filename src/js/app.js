2be6fbe7fde47b23d3ea97f5cf41c14688cf1ce0
lionel-prats <lionel.prats.c@gmail.com>
2023-05-22 01:13:11 -0300

    seccion 52 - video 492

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-alert("desde app.js");
+let paso = 1;
+
+// 'DOMContentLoaded' es un evento en JavaScript que se dispara cuando el árbol DOM (Documento Object Model) de una página web ha sido completamente cargado y parseado. El evento 'DOMContentLoaded' ocurre después de que el navegador ha construido el árbol DOM a partir del código HTML de la página, pero antes de que se hayan descargado todos los recursos externos, como imágenes o scripts adicionales.
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp(){
+    tabs(); // muestra y oculta las secciones de /cita segun los clicks en <div class="tabs">
+}
+function mostrarSeccion(){
+    console.log('mostrando seccion');
+}
+function tabs(){
+    const botones = document.querySelectorAll('.tabs button');
+    botones.forEach( boton => {
+        boton.addEventListener('click', function(e){
+            paso = parseInt(e.target.dataset.paso);
+            mostrarSeccion();
+        })
+    });
+}
