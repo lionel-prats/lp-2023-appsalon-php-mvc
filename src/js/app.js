e15e7cd0279cb1019a287123949852eeba942385
lionel-prats <lionel.prats.c@gmail.com>
2023-05-24 00:32:34 -0300

    seccion 52 - video 495

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function iniciarApp(){
-    mostrarSeccion(); 
+    mostrarSeccion(); // ejecutamos esta funcion al principio para que se arranque mostrando la seccion servicios
     tabs(); // muestra y oculta las secciones de /cita segun los clicks en <div class="tabs">
+    botonesPaginador() // agrega o quita los botones del paginador 
 }
 
 function mostrarSeccion(){
@@ -41,6 +42,23 @@ function tabs(){
         boton.addEventListener('click', function(e){
             paso = parseInt(e.target.dataset.paso);
             mostrarSeccion();
+            botonesPaginador();
         })
     });
+}
+
+function botonesPaginador(){
+    const paginaAnterior = document.querySelector('#anterior');
+    const paginaSiguiente = document.querySelector('#siguiente');
+
+    if(paso === 1) {
+        paginaAnterior.classList.add('ocultar');    
+        paginaSiguiente.classList.remove('ocultar');
+    } else if(paso === 3) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');    
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
 }
