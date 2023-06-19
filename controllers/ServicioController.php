7e3d8ffb3964ed21d6339fa25589e36c7570f314
lionel-prats <lionel.prats.c@gmail.com>
2023-06-19 19:18:44 -0300

    seccion 56 - video 545

---
+++ controllers/ServicioController.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Controllers;
+
+//use MVC\Router;
+
+class ServicioController {
+    public static function index(/* Router */ $router){
+        $router->render("/servicios/index", [
+            
+        ]);
+    }
+    public static function crear(/* Router */ $router){
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            
+        }
+    }
+    public static function actualizar(/* Router */ $router){
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+        }
+    }
+    public static function eliminar(){
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use Controllers\APIController;
 use Controllers\CitaController;
 use Controllers\AdminController;
 use Controllers\LoginController;
+use Controllers\ServicioController;
 
 $router = new Router();
 
@@ -38,6 +39,15 @@ $router->get("/api/pruebas", [APIController::class, "pruebas"]);
 $router->get("/api/servicios", [APIController::class, "index"]); // retorna los registros de la tabla "servicios"
 $router->post("/api/citas", [APIController::class, "guardar"]); // endpoint que va a procesar las reservas de citas en el back (VIDEO 518)
 $router->post("/api/eliminar", [APIController::class, "eliminar"]); // emdpoint para que el administrador pueda eliminar citas desde el panel de administrador
+
+// CRUD de servicios (solo para el administrador) (empieza en el VIDEO 545)
+$router->get("/servicios", [ServicioController::class, "index"]);
+$router->get("/servicios/crear", [ServicioController::class, "crear"]);
+$router->post("/servicios/crear", [ServicioController::class, "crear"]);
+$router->get("/servicios/actualizar", [ServicioController::class, "actualizar"]);
+$router->post("/servicios/actualizar", [ServicioController::class, "actualizar"]);
+$router->post("/servicios/eliminar", [ServicioController::class, "eliminar"]);
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
