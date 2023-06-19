70f9ef187f612e49717c4649c65705d19d230aa0
lionel-prats <lionel.prats.c@gmail.com>
2023-06-18 22:10:48 -0300

    seccion 56 - video 539

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -13,6 +13,13 @@ class AdminController {
         $fecha = date("Y-m-d");
         //debuguear($fecha);
 
+        /* $consulta = "
+            SELECT citas.id, citas.hora, CONCAT(usuarios.nombre, ' ', usuarios.apellido) AS cliente, usuarios.email, usuarios.telefono, servicios.nombre AS servicio, servicios.precio 
+            FROM citas
+            LEFT OUTER JOIN usuarios ON usuarios.id = citas.usuarioId 
+            LEFT OUTER JOIN citasservicios ON citasservicios.citaId = citas.id  
+            LEFT OUTER JOIN servicios ON servicios.id = citasservicios.servicioId  
+        "; */
         $consulta = "
             SELECT citas.id, citas.hora, CONCAT(usuarios.nombre, ' ', usuarios.apellido) AS cliente, usuarios.email, usuarios.telefono, servicios.nombre AS servicio, servicios.precio 
             FROM citas
@@ -21,7 +28,6 @@ class AdminController {
             LEFT OUTER JOIN servicios ON servicios.id = citasservicios.servicioId  
             WHERE fecha = '$fecha'
         ";
-        //WHERE fecha = '2023-06-19'
     
         $citas = AdminCita::SQL($consulta); // ver explicacion detallada en z.notas.txt, VIDEO 534
 
