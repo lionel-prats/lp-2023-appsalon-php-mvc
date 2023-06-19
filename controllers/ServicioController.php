61d7e17126b6ea9435a843bddf29a6d46bf5536c
lionel-prats <lionel.prats.c@gmail.com>
2023-06-19 19:32:01 -0300

    seccion 56 - video 546

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -7,18 +7,24 @@ namespace Controllers;
 class ServicioController {
     public static function index(/* Router */ $router){
         $router->render("/servicios/index", [
-            
+            "nombre" => $_SESSION["nombre"]
         ]);
     }
     public static function crear(/* Router */ $router){
         if($_SERVER["REQUEST_METHOD"] === "POST"){
             
         }
+        $router->render("/servicios/crear", [
+            "nombre" => $_SESSION["nombre"]
+        ]);
     }
     public static function actualizar(/* Router */ $router){
         if($_SERVER["REQUEST_METHOD"] === "POST"){
 
         }
+        $router->render("/servicios/actualizar", [
+            "nombre" => $_SESSION["nombre"]
+        ]);
     }
     public static function eliminar(){
         if($_SERVER["REQUEST_METHOD"] === "POST"){
