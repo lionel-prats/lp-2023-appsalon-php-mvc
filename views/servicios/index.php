8cf0b0db13c43d2bd771cfe71370dac841071e2f
lionel-prats <lionel.prats.c@gmail.com>
2023-06-19 22:25:19 -0300

    seccion 56 - video 550

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -4,8 +4,15 @@
 <ul class="servicios">
     <?php foreach($servicios as $servicio): ?>  
         <li>
-            <p>Nombre: <span><?php echo $servicio->nombre?></span></p>
-            <p>Precio: <span>$<?php echo $servicio->precio?></span></p>
+            <p>Nombre: <span><?php echo $servicio->nombre; ?></span></p>
+            <p>Precio: <span>$<?php echo $servicio->precio; ?></span></p>
+            <div class="acciones">
+                <a class="boton" href="/servicios/actualizar?id=<?php echo $servicio->id; ?>">Actualizar</a>
+                <form action="/servicios/eliminar" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $servicio->id; ?>">
+                    <input type="submit" class="boton-eliminar" value="Borrar">
+                </form>
+            </div>
         </li>
     <?php endforeach; ?>
 </ul>
