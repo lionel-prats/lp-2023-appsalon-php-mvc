0cac4172b78ffc82b8a36a568785d9dfb5483987
lionel-prats <lionel.prats.c@gmail.com>
2023-06-20 17:09:57 -0300

    seccion 57 - video 557

--- includes/app.php
+++ includes/app.php
@@ -1,8 +1,19 @@
 <?php 
 
+// autoload -> carga automaticamente las dependencias que descargué utilizando composer (VIDEO 556)
+require __DIR__ . '/../vendor/autoload.php';
+
+// instancia denpendencia instalada vlucas/phpdotenv
+// como primer parametro le pasamos __DIR__, para indicar la ubicacion del .env (database.php y .env se encuentran en la misma ubicacion dentro del proyecto) (VIDEO 556)
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+// con esta funcion prevenimos que nos marque error si el .ENV no existe (en el servidor de produccion no va a existir el .env, ya que las variables de entorno se van a inyectar por un a traves de un panel especial) (VIDEO 556)
+$dotenv->safeload(); 
+
+
 require 'funciones.php';
 require 'database.php';
-require __DIR__ . '/../vendor/autoload.php';
+
 
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
